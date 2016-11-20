d411da7d921745c8b8e53c8095dfbba2498f6027
Adam Antinoo <haddockgit@gmail.com>
2016-11-20 21:20:38 +0100

    -WIP. Adding dislog to set the number of runs for a fit job.

---
+++ EIA/src/main/org/dimensinfin/evedroid/dialog/FittingRunsDialog.java
@@ -0,0 +1,95 @@
+//	PROJECT:        NeoCom.Android (NEOC.A)
+//	AUTHORS:        Adam Antinoo - adamantinoo.git@gmail.com
+//	COPYRIGHT:      (c) 2013-2016 by Dimensinfin Industries, all rights reserved.
+//	ENVIRONMENT:		Android API16.
+//	DESCRIPTION:		Application to get access to CCP api information and help manage industrial activities
+//									for characters and corporations at Eve Online. The set is composed of some projects
+//									with implementation for Android and for an AngularJS web interface based on REST
+//									services on Sprint Boot Cloud.
+package org.dimensinfin.evedroid.dialog;
+
+import java.util.logging.Logger;
+
+import org.dimensinfin.android.mvc.activity.ADialogCallback;
+import org.dimensinfin.evedroid.R;
+import org.dimensinfin.evedroid.part.FittingPart;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+// - CLASS IMPLEMENTATION ...................................................................................
+public class FittingRunsDialog extends DialogFragment {
+	// - S T A T I C - S E C T I O N ..........................................................................
+	private static Logger		logger						= Logger.getLogger("FittingRunsDialog");
+
+	// - F I E L D - S E C T I O N ............................................................................
+	private View						_dialogContainer	= null;
+	private ADialogCallback	_dialogCallback		= null;
+	private FittingPart			_fittingPart			= null;
+	/**
+	 * The number of runs selected by the user. There is a top limit that is the less of the manufacturable
+	 * count or the number of runs available on the blueprints.
+	 */
+	private EditText				_runsCount				= null;
+	private TextView				_fittingName			= null;
+	private TextView				_errorMessage			= null;
+
+	// - W O R K   V A R I A B L E S
+	private final int				_runs							= 0;
+
+	// - C O N S T R U C T O R - S E C T I O N ................................................................
+	public FittingRunsDialog() {
+	}
+
+	// - M E T H O D - S E C T I O N ..........................................................................
+	@Override
+	public Dialog onCreateDialog(final Bundle savedInstanceState) {
+		// Create the dialog and all its elements.
+		AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+		// Inflate and set the layout for the dialog
+		LayoutInflater inflater = getActivity().getLayoutInflater();
+		_dialogContainer = inflater.inflate(R.layout.dialog_jobruns, null);
+
+		initializeViews();
+		setupContents();
+
+	}
+
+	public void setDialogCallback(final ADialogCallback callback) {
+		if (null != callback) {
+			_dialogCallback = callback;
+		}
+	}
+
+	public void setFittingPart(final FittingPart part) {
+		_fittingPart = part;
+	}
+
+	private void initializeViews() {
+		// Get access to the dialog UI components.
+		_fittingName = (TextView) _dialogContainer.findViewById(R.id.fittingName);
+		_runsCount = (EditText) _dialogContainer.findViewById(R.id.runsCount);
+		//		_blueprintCount = (TextView) _dialogContainer.findViewById(R.id.blueprintCount);
+		//		_blueprintRuns = (TextView) _dialogContainer.findViewById(R.id.blueprintRuns);
+		//		_blueprintMETE = (TextView) _dialogContainer.findViewById(R.id.blueprintMETE);
+		//		_jobCount = (TextView) _dialogContainer.findViewById(R.id.jobCount);
+		//		_jobDuration = (TextView) _dialogContainer.findViewById(R.id.jobDuration);
+		_errorMessage = (TextView) _dialogContainer.findViewById(R.id.errorMessage);
+	}
+
+	private void setupContents() {
+		// Set the text of the informative fields from the blueprint part.
+		_fittingName.setText(_fittingPart.getName());
+		//		_blueprintCount.setText(_blueprintPart.get_blueprintCount());
+		//		_blueprintRuns.setText("[" + _blueprintPart.getRuns() + "]");
+		//		_blueprintMETE.setText(_blueprintPart.get_blueprintMETE());
+	}
+}
+
+// - UNUSED CODE ............................................................................................
--- EIA/src/main/org/dimensinfin/evedroid/part/FittingPart.java
+++ EIA/src/main/org/dimensinfin/evedroid/part/FittingPart.java
@@ -13,13 +13,15 @@ import java.util.logging.Logger;
 import org.dimensinfin.android.mvc.activity.ADialogCallback;
 import org.dimensinfin.android.mvc.core.AbstractHolder;
 import org.dimensinfin.android.mvc.interfaces.IMenuActionTarget;
+import org.dimensinfin.evedroid.activity.FittingActivity;
 import org.dimensinfin.evedroid.activity.JobDirectorActivity;
 import org.dimensinfin.evedroid.constant.AppWideConstants;
 import org.dimensinfin.evedroid.core.EveAbstractPart;
-import org.dimensinfin.evedroid.dialog.JobRunsDialog;
+import org.dimensinfin.evedroid.dialog.FittingRunsDialog;
 import org.dimensinfin.evedroid.industry.JobManager;
 import org.dimensinfin.evedroid.interfaces.INamedPart;
 
+import android.app.DialogFragment;
 import android.content.Intent;
 import android.view.ContextMenu;
 import android.view.ContextMenu.ContextMenuInfo;
@@ -58,18 +60,15 @@ public class FittingPart extends EveAbstractPart implements INamedPart, OnClickL
 	 */
 	// REFACTOR Removed during the DataSource integration
 	public void onCreateContextMenu(final ContextMenu menu, final View view, final ContextMenuInfo menuInfo) {
-		Log.i("EVEI", ">> BlueprintPart.onCreateContextMenu");
-		// PagerFragment frag = (PagerFragment) getFragment();
-		// For blueprints the menu depends on the renderer selected.
-		if ((getRenderMode() == AppWideConstants.rendermodes.RENDER_BLUEPRINTINDUSTRYHEADER)
-				|| (getRenderMode() == AppWideConstants.rendermodes.RENDER_BLUEPRINTINVENTIONHEADER)) {
-			final JobRunsDialog dialog = new JobRunsDialog();
-			dialog.setBlueprintPart(this);
-			final BlueprintPart self = this;
+		logger.info(">> [FittingPart.onCreateContextMenu]");
+		// Check the renderer to see if I have to show the dialog or not. Only valid if in a header.
+		if (getRenderMode() == AppWideConstants.rendermodes.RENDER_FITTINGHEADER) {
+			final FittingRunsDialog dialog = new FittingRunsDialog();
+			dialog.setFittingPart(this);
+			//			final BlueprintPart self = this;
 			// PagerFragment frag = (PagerFragment) getFragment();
 			// dialog.setFragment(frag);
 			dialog.setDialogCallback(new ADialogCallback() {
-
 				@Override
 				public void onDialogNegativeClick(final DialogFragment dialog) {
 				}
@@ -77,7 +76,14 @@ public class FittingPart extends EveAbstractPart implements INamedPart, OnClickL
 				@Override
 				public void onDialogPositiveClick(final DialogFragment dialog) {
 					// Get the number of runs selected by the user.
-					final int runs = ((JobRunsDialog) dialog).getRuns();
+					final int runs = ((FittingRunsDialog) dialog).getRuns();
+
+					// Open the Fitting Activity
+					final Intent intent = new Intent(getActivity(), FittingActivity.class);
+					intent.putExtra(AppWideConstants.extras.EXTRA_EVECHARACTERID, getPilot().getCharacterID());
+					intent.putExtra(AppWideConstants.EExtras.FITTINGID.name(), label);
+					getActivity().startActivity(intent);
+
 					// Verify with the number of runs the number of blueprints
 					// used.
 					Toast.makeText(getActivity(), "Selected Runs: " + runs, Toast.LENGTH_LONG).show();
@@ -91,7 +97,7 @@ public class FittingPart extends EveAbstractPart implements INamedPart, OnClickL
 			getActivity().getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
 			dialog.show(getActivity().getFragmentManager(), "JobRunsDialog");
 		}
-		Log.i("EVEI", "<< BlueprintPart.onCreateContextMenu");
+		logger.info("<< [FittingPart.onCreateContextMenu]");
 	}
 
 	@Override
