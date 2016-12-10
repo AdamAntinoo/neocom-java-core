4d1993ed677129f45a665557711c6155f9cf340b
Adam Antinoo <haddockgit@gmail.com>
2016-12-10 23:25:42 +0100

    -Updating the initialization to the new static model. Needs integration with other branches.

--- EIA/src/main/org/dimensinfin/evedroid/activity/core/SplashActivity.java
+++ EIA/src/main/org/dimensinfin/evedroid/activity/core/SplashActivity.java
@@ -6,16 +6,10 @@
 
 package org.dimensinfin.evedroid.activity.core;
 
-// - IMPORT SECTION .........................................................................................
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.Set;
@@ -26,10 +20,7 @@ import org.dimensinfin.evedroid.R;
 import org.dimensinfin.evedroid.activity.PilotListActivity;
 import org.dimensinfin.evedroid.connector.AppConnector;
 import org.dimensinfin.evedroid.constant.AppWideConstants;
-import org.dimensinfin.evedroid.model.NeoComApiKey;
 import org.dimensinfin.evedroid.model.EveChar;
-import org.dimensinfin.evedroid.model.EveCharCore;
-import org.dimensinfin.evedroid.model.NeoComApiKey;
 import org.dimensinfin.evedroid.storage.AppModelStore;
 
 import com.beimin.eveapi.parser.ApiAuthorization;
@@ -63,7 +54,6 @@ public class SplashActivity extends Activity {
 	private class EveDroidInitialization extends AsyncTask<Void, Void, Boolean> {
 		private static final boolean	showProgress	= false;
 		// - F I E L D - S E C T I O N ............................................................................
-		//		private EVEDroidApp						app						= null;
 		private Activity							_activity			= null;
 
 		// - C O N S T R U C T O R - S E C T I O N ................................................................
@@ -73,13 +63,12 @@ public class SplashActivity extends Activity {
 
 		@Override
 		protected Boolean doInBackground(final Void... entry) {
-			// Just check that the files exist and if not load the Api list and their characters.
 			updateStateLabel("Checking application database...");
-			// STEP 03. Check required files availability on app directory.
+			// STEP 01. Check required files availability on app directory.
 			createAppDir();
 			createCacheDirectories();
-			logger.info(">> EveDroidInitialization.STEP 03. App Directories created");
-			// STEP 04. Check existence of required files.
+			logger.info(">> EveDroidInitialization.STEP 01. App Directories created");
+			// STEP 02. Check existence of required files.
 			if (!checkAppFile(AppConnector.getResourceString(R.string.ccpdatabasefilename))) {
 				// Initial item database is not on place. Copy one from the assets.
 				copyFromAssets(R.string.ccpdatabasefilename);
@@ -88,15 +77,18 @@ public class SplashActivity extends Activity {
 				// Initial item database is not on place. Copy one from the assets.
 				copyFromAssets(R.string.apikeysfilename);
 			}
-			logger.info(">> EveDroidInitialization.STEP 04. Required files on place");
+			logger.info(">> EveDroidInitialization.STEP 02. Required files on place");
 
 			updateStateLabel("Loading user data...");
+			// STEP 03. Initialize the Model store and force a refresh from the api list file.
+			AppModelStore.initialize();
+
 			// STEP 08. Check API file.
-			if (!checkAppFile(AppConnector.getResourceString(R.string.apikeysfilename))) {
-				logger.info("-- EveDroidInitialization.STEP 06. No API List. Needs API introduction");
-				startActivity(new Intent(_activity, AddNewAPIActivity.class));
-				return false;
-			}
+			//			if (!checkAppFile(AppConnector.getResourceString(R.string.apikeysfilename))) {
+			//				logger.info("-- EveDroidInitialization.STEP 06. No API List. Needs API introduction");
+			//				startActivity(new Intent(_activity, AddNewAPIActivity.class));
+			//				return false;
+			//			}
 			AppModelStore store = EVEDroidApp.getAppStore();
 			//				// Try to read the data. If this fails we have to force an update or leave the user to do that.
 			//				status = EVEDroidApp.getSingletonApp().getAppStore().restore();
@@ -105,7 +97,7 @@ public class SplashActivity extends Activity {
 			logger.info(">> EveDroidInitialization.STEP 08. API list available");
 			updateStateLabel("Downloading data from CCP...");
 			// STEP 09. Refresh data from CCP.	
-			readApiKeys();
+			//			readApiKeys();
 			logger.info(">> EveDroidInitialization.STEP 09. API list refreshed");
 			EVEDroidApp.getSingletonApp().startTimer();
 			return true;
@@ -336,42 +328,42 @@ public class SplashActivity extends Activity {
 
 		}
 
-		private void readApiKeys() {
-			logger.info(">> EveDroidInitialization.readApiKeys");
-			try {
-				// Read the contents of the character information.
-				final File characterFile = AppConnector.getStorageConnector()
-						.accessAppStorage(AppConnector.getResourceString(R.string.apikeysfilename));
-				InputStream is = new BufferedInputStream(new FileInputStream(characterFile));
-				BufferedReader br = new BufferedReader(new InputStreamReader(is, "UTF-8"));
-				String line = br.readLine();
-				while (null != line) {
-					try {
-						String[] parts = line.split(":");
-						String key = parts[0];
-						String validationcode = parts[1];
-						int keynumber = Integer.parseInt(key);
-						logger.info("-- Inserting API key " + keynumber);
-						//						APIKey api = new APIKey(keynumber, validationcode);
-						NeoComApiKey api = NeoComApiKey.build(keynumber, validationcode);
-						EVEDroidApp.getAppStore().addApiKey(api);
-					} catch (NumberFormatException nfex) {
-					} catch (Exception ex) {
-						ex.printStackTrace();
-					}
-					line = br.readLine();
-				}
-				if (null != br) {
-					br.close();
-				}
-			} catch (FileNotFoundException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
+		//		private void readApiKeys() {
+		//			logger.info(">> EveDroidInitialization.readApiKeys");
+		//			try {
+		//				// Read the contents of the character information.
+		//				final File characterFile = AppConnector.getStorageConnector()
+		//						.accessAppStorage(AppConnector.getResourceString(R.string.apikeysfilename));
+		//				InputStream is = new BufferedInputStream(new FileInputStream(characterFile));
+		//				BufferedReader br = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+		//				String line = br.readLine();
+		//				while (null != line) {
+		//					try {
+		//						String[] parts = line.split(":");
+		//						String key = parts[0];
+		//						String validationcode = parts[1];
+		//						int keynumber = Integer.parseInt(key);
+		//						logger.info("-- Inserting API key " + keynumber);
+		//						//						APIKey api = new APIKey(keynumber, validationcode);
+		//						NeoComApiKey api = NeoComApiKey.build(keynumber, validationcode);
+		//						EVEDroidApp.getAppStore().addApiKey(api);
+		//					} catch (NumberFormatException nfex) {
+		//					} catch (Exception ex) {
+		//						ex.printStackTrace();
+		//					}
+		//					line = br.readLine();
+		//				}
+		//				if (null != br) {
+		//					br.close();
+		//				}
+		//			} catch (FileNotFoundException e) {
+		//				// TODO Auto-generated catch block
+		//				e.printStackTrace();
+		//			} catch (IOException e) {
+		//				// TODO Auto-generated catch block
+		//				e.printStackTrace();
+		//			}
+		//		}
 
 		private void updateStateLabel(final String newLabel) {
 			runOnUiThread(new Runnable() {
