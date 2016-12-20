c691aaef88adf493d28eaac32ccad0119e81770c
unknown <ldediego@EBS-100648470.cosmos.es.ftgroup>
2016-12-20 11:29:35 +0100

    -[BUG] The policy returned an empty list so it sutted the output completely and the view was empty.

--- AndroidMVC/src/org/dimensinfin/android/mvc/core/RootPart.java
+++ AndroidMVC/src/org/dimensinfin/android/mvc/core/RootPart.java
@@ -20,46 +20,15 @@ public class RootPart extends AbstractPart {
 	// - S T A T I C - S E C T I O N ..........................................................................
 	private static final long	serialVersionUID	= -8085543451527813221L;
 	private static Logger			logger						= Logger.getLogger("SeparatorPart");
-	//	private IPartFactory			_factory					= null;
 
 	// - F I E L D - S E C T I O N ............................................................................
 
 	// - C O N S T R U C T O R - S E C T I O N ................................................................
 	public RootPart(final RootNode node, final IPartFactory factory) {
 		super(node, factory);
-		//		_factory = factory;
 	}
 
 	// - M E T H O D - S E C T I O N ..........................................................................
-	//	/**
-	//	 * This method is the one that will run over the part and its children to detect what elements (Parts) go to
-	//	 * the result list and from it to the ListView to be presented on the display.
-	//	 * 
-	//	 * @return
-	//	 */
-	//	@Override
-	//	public ArrayList<AbstractAndroidPart> collaborate2View() {
-	//		RootPart.logger.info(">> [RootPart.collaborate2View]");
-	//		ArrayList<AbstractAndroidPart> childrenParts = new ArrayList<AbstractAndroidPart>();
-	//		Vector<AbstractPropertyChanger> v = this.getChildren();
-	//		for (AbstractPropertyChanger node : v)
-	//			childrenParts.add((AbstractAndroidPart) node);
-	//		ArrayList<AbstractAndroidPart> result = new ArrayList<AbstractAndroidPart>(childrenParts.size());
-	//		for (AbstractAndroidPart child : childrenParts)
-	//			if (child instanceof AbstractAndroidPart) {
-	//				AbstractAndroidPart node = child;
-	//				// Add this node only if not empty or if empty but marked as viewable.
-	//				IGEFNode partModel = node.getModel();
-	//				if (partModel instanceof INeoComNode) {
-	//					INeoComNode model = (INeoComNode) partModel;
-	//					if (model.renderWhenEmpty()) result.add(child);
-	//				} else
-	//					RootPart.logger.warning("WW [RootPart.collaborate2View]> Part not implementing INeoComNode");
-	//				result.addAll(node.collaborate2View());
-	//			}
-	//		return result;
-	//	}
-
 	public long getModelID() {
 		return this.getModel().getClass().hashCode();
 	}
@@ -69,68 +38,16 @@ public class RootPart extends AbstractPart {
 		return this;
 	}
 
-	//	public void invalidate() {
-	//		// TODO Auto-generated method stub
-	//
-	//	}
-
 	public void needsRedraw() {
 	}
 
-	//	@Override
-	//	protected List<IGEFNode> collaborate2Model() {
-	//		Vector<IGEFNode> c = this.getModel().getChildren();
-	//		return c;
-	//	}
-
-	//	/**
-	//	 * Create the Part for the model object received. We have then to have access to the Factory from the root
-	//	 * element and all the other parts should have a reference to the root to be able to do the same.
-	//	 */
-	//	@Override
-	//	protected IEditPart createChild(final Object model) {
-	//		IPartFactory factory = this.getRoot().getPartFactory();
-	//		IEditPart part = factory.createPart((IGEFNode) model);
-	//		part.setParent(this);
-	//		return part;
-	//	}
-
-	//	protected IPartFactory getFactory() {
-	//		return _factory;
-	//	}
-
+	/**
+	 * Returns the same input because the core Root has no policies.
+	 */
 	@Override
 	public Vector<IPart> runPolicies(final Vector<IPart> targets) {
-		// TODO Auto-generated method stub
-		return null;
+		return targets;
 	}
-
-	//	/**
-	//	 * A RooPart has no visuals, but other Parts may have.
-	//	 */
-	//	protected void removeChildVisual(final IEditPart child) {
-	//	}
-
-	// public String get_title() {
-	// return getCastedModel().getTitle();
-	// }
-	//
-	// public Separator getCastedModel() {
-	// return (Separator) getModel();
-	// }
-	//
-	// @Override
-	// public long getModelID() {
-	// return GregorianCalendar.getInstance().getTimeInMillis();
-	// }
-	//
-	// @Override
-	// protected AbstractHolder selectHolder() {
-	// // Get the proper holder from the render mode.
-	// return new SeparatorHolder(this, getActivity());
-	// }
-
 }
 
-// - UNUSED CODE
-// ............................................................................................
+// - UNUSED CODE ............................................................................................
