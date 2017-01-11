056025e411a0c4d317e0047cd8f5bd075b8d6033
Adam Antinoo <adamantinoo.git@gmail.com>
2017-01-11 23:10:28 +0100

    -Adding the database connection. This code can be imported from older releases that used pure java code.

--- POC-Planetary/src/org/dimensinfin/eveonline/poc/planetary/PlanetaryProcessor.java
+++ POC-Planetary/src/org/dimensinfin/eveonline/poc/planetary/PlanetaryProcessor.java
@@ -11,6 +11,10 @@ import java.util.logging.Logger;
 
 import org.dimensinfin.eveonline.neocom.industry.Resource;
 
+import com.j256.ormlite.dao.Dao;
+import com.j256.ormlite.jdbc.JdbcConnectionSource;
+import com.j256.ormlite.support.ConnectionSource;
+
 // - CLASS IMPLEMENTATION ...................................................................................
 public class PlanetaryProcessor {
 	// - S T A T I C - S E C T I O N ..........................................................................
@@ -23,6 +27,8 @@ public class PlanetaryProcessor {
 	private static final String	SELECT_RAW_PRODUCTRESULT_INV	= "SELECT ps.schematicName AS productName, ps.cycleTime AS cycleTime, pstm.quantity AS inputQuantity"
 			+ " FROM  planetSchematicsTypeMap pstm" + " WHERE pstm.isInput AND   pstm.typeID = ?"
 			+ " LEFT OUTER JOIN planetSchematics ps ON ps.schematicID = pstm.schematicID";
+
+	private final static String	DATABASE_URL									= "jdbc:h2:mem:account";
 
 	// - M E T H O D - S E C T I O N ..........................................................................
 	public static void process(Resource inResource) {
@@ -46,7 +52,12 @@ public class PlanetaryProcessor {
 	}
 
 	// - F I E L D - S E C T I O N ............................................................................
-	private SQLiteDatabase ccpDatabase = null;
+	private SQLiteDatabase				ccpDatabase				= null;
+
+	private Dao<Account, Integer>	accountDao;
+	String												databaseUrl				= "jdbc:h2:mem:account";
+	// create a connection source to our database
+	ConnectionSource							connectionSource	= new JdbcConnectionSource(databaseUrl);
 
 	// - C O N S T R U C T O R - S E C T I O N ................................................................
 	public PlanetaryProcessor() {
