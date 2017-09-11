a29acdbf104562865ed5565af8aa6ab904580df6
Adam Antinoo <adamantinoo.git@gmail.com>
2017-09-11 16:36:50 +0200

    -Added the new PRIORITY comparator.

--- NeoCom.Model/src/main/org/dimensinfin/eveonline/neocom/core/ComparatorFactory.java
+++ NeoCom.Model/src/main/org/dimensinfin/eveonline/neocom/core/ComparatorFactory.java
@@ -10,9 +10,6 @@
 package org.dimensinfin.eveonline.neocom.core;
 
 import java.util.Comparator;
-// - IMPORT SECTION .........................................................................................
-import java.util.Date;
-import java.util.List;
 import java.util.logging.Logger;
 
 import org.dimensinfin.core.model.AbstractPropertyChanger;
@@ -21,45 +18,46 @@ import org.dimensinfin.eveonline.neocom.industry.JobQueue;
 import org.dimensinfin.eveonline.neocom.industry.Resource;
 import org.dimensinfin.eveonline.neocom.interfaces.IWeigthedNode;
 import org.dimensinfin.eveonline.neocom.model.NeoComAsset;
-import org.joda.time.DateTime;
-import org.joda.time.DateTimeZone;
+import org.dimensinfin.eveonline.neocom.services.PendingRequestEntry;
+
 // - CLASS IMPLEMENTATION ...................................................................................
 public class ComparatorFactory {
 	// - S T A T I C - S E C T I O N ..........................................................................
 	private static Logger logger = Logger.getLogger("ComparatorFactory");
-	public static Comparator<AbstractPropertyChanger> createComparator(EComparatorField code) {
+
+	public static Comparator<AbstractPropertyChanger> createComparator(final EComparatorField code) {
 		Comparator<AbstractPropertyChanger> comparator = new Comparator<AbstractPropertyChanger>() {
 			public int compare(final AbstractPropertyChanger left, final AbstractPropertyChanger right) {
 				return 0;
 			}
 		};
 		switch (code) {
-//			case NAME:
-//				comparator = new Comparator<AbstractPropertyChanger>() {
-//					public int compare(final AbstractPropertyChanger left, final AbstractPropertyChanger right) {
-//						String leftField = null;
-//						String rightField = null;
-//						if (left instanceof INamedPart) {
-//							leftField = ((INamedPart) left).getName();
-//						}
-//						if (right instanceof INamedPart) {
-//							rightField = ((INamedPart) right).getName();
-//						}
-//						if (left instanceof INamed) {
-//							leftField = ((INamed) left).getOrderingName();
-//						}
-//						if (right instanceof INamed) {
-//							rightField = ((INamed) right).getOrderingName();
-//						}
-//
-//						if (null == leftField) return 1;
-//						if (null == rightField) return -1;
-//						if ("" == leftField) return 1;
-//						if ("" == rightField) return -1;
-//						return leftField.compareTo(rightField);
-//					}
-//				};
-//				break;
+			//			case NAME:
+			//				comparator = new Comparator<AbstractPropertyChanger>() {
+			//					public int compare(final AbstractPropertyChanger left, final AbstractPropertyChanger right) {
+			//						String leftField = null;
+			//						String rightField = null;
+			//						if (left instanceof INamedPart) {
+			//							leftField = ((INamedPart) left).getName();
+			//						}
+			//						if (right instanceof INamedPart) {
+			//							rightField = ((INamedPart) right).getName();
+			//						}
+			//						if (left instanceof INamed) {
+			//							leftField = ((INamed) left).getOrderingName();
+			//						}
+			//						if (right instanceof INamed) {
+			//							rightField = ((INamed) right).getOrderingName();
+			//						}
+			//
+			//						if (null == leftField) return 1;
+			//						if (null == rightField) return -1;
+			//						if ("" == leftField) return 1;
+			//						if ("" == rightField) return -1;
+			//						return leftField.compareTo(rightField);
+			//					}
+			//				};
+			//				break;
 			case ASSET_COUNT:
 				comparator = new Comparator<AbstractPropertyChanger>() {
 					public int compare(final AbstractPropertyChanger left, final AbstractPropertyChanger right) {
@@ -80,46 +78,46 @@ public class ComparatorFactory {
 					}
 				};
 				break;
-//			case AppWideConstants.comparators.COMPARATOR_ITEM_TYPE:
-//				comparator = new Comparator<AbstractPropertyChanger>() {
-//					public int compare(final AbstractPropertyChanger left, final AbstractPropertyChanger right) {
-//						int leftField = -1;
-//						int rightField = -1;
-//						// if (left instanceof BlueprintPart) leftField = 100;
-//						if (left instanceof AssetPart) {
-//							leftField = 0;
-//						}
-//						if (left instanceof ShipPart) {
-//							leftField = 200;
-//						}
-//						if (left instanceof ContainerPart) {
-//							leftField = -300;
-//						}
-//
-//						// if (right instanceof BlueprintPart) rightField = 100;
-//						if (right instanceof AssetPart) {
-//							rightField = 0;
-//						}
-//						if (right instanceof ShipPart) {
-//							rightField = 200;
-//						}
-//						if (right instanceof ContainerPart) {
-//							rightField = -300;
-//						}
-//
-//						if (leftField < rightField) return -1;
-//						if (leftField > rightField) return 1;
-//						return 0;
-//					}
-//				};
-//				break;
+			//			case AppWideConstants.comparators.COMPARATOR_ITEM_TYPE:
+			//				comparator = new Comparator<AbstractPropertyChanger>() {
+			//					public int compare(final AbstractPropertyChanger left, final AbstractPropertyChanger right) {
+			//						int leftField = -1;
+			//						int rightField = -1;
+			//						// if (left instanceof BlueprintPart) leftField = 100;
+			//						if (left instanceof AssetPart) {
+			//							leftField = 0;
+			//						}
+			//						if (left instanceof ShipPart) {
+			//							leftField = 200;
+			//						}
+			//						if (left instanceof ContainerPart) {
+			//							leftField = -300;
+			//						}
+			//
+			//						// if (right instanceof BlueprintPart) rightField = 100;
+			//						if (right instanceof AssetPart) {
+			//							rightField = 0;
+			//						}
+			//						if (right instanceof ShipPart) {
+			//							rightField = 200;
+			//						}
+			//						if (right instanceof ContainerPart) {
+			//							rightField = -300;
+			//						}
+			//
+			//						if (leftField < rightField) return -1;
+			//						if (leftField > rightField) return 1;
+			//						return 0;
+			//					}
+			//				};
+			//				break;
 			case RESOURCE_TYPE:
 				comparator = new Comparator<AbstractPropertyChanger>() {
 					public int compare(final AbstractPropertyChanger left, final AbstractPropertyChanger right) {
 						int leftField = -1;
 						int rightField = -1;
 						if (left instanceof Resource) {
-							final Resource resource =(Resource) left;
+							final Resource resource = (Resource) left;
 							leftField = 0;
 							if (resource.getCategory().equalsIgnoreCase("Material")) {
 								leftField = -300;
@@ -158,86 +156,86 @@ public class ComparatorFactory {
 					}
 				};
 				break;
-//			case AppWideConstants.comparators.COMPARATOR_APIID_ASC:
-//				comparator = new Comparator<AbstractPropertyChanger>() {
-//					public int compare(final AbstractPropertyChanger left, final AbstractPropertyChanger right) {
-//						long leftField = -1;
-//						long rightField = -1;
-//						if (left instanceof APIKeyPart) {
-//							final APIKey intermediate = ((APIKeyPart) left).getCastedModel();
-//							leftField = intermediate.getKeyID();
-//						}
-//
-//						if (right instanceof APIKeyPart) {
-//							final APIKey intermediate = ((APIKeyPart) right).getCastedModel();
-//							rightField = intermediate.getKeyID();
-//						}
-//						if (leftField < rightField) return -1;
-//						if (leftField > rightField) return 1;
-//						return 0;
-//					}
-//				};
-//				break;
-//			case AppWideConstants.comparators.COMPARATOR_APIID_DESC:
-//				comparator = new Comparator<AbstractPropertyChanger>() {
-//					public int compare(final AbstractPropertyChanger left, final AbstractPropertyChanger right) {
-//						long leftField = -1;
-//						long rightField = -1;
-//						if (left instanceof APIKeyPart) {
-//							final APIKey intermediate = ((APIKeyPart) left).getCastedModel();
-//							leftField = intermediate.getKeyID();
-//						}
-//
-//						if (right instanceof APIKeyPart) {
-//							final APIKey intermediate = ((APIKeyPart) right).getCastedModel();
-//							rightField = intermediate.getKeyID();
-//						}
-//						if (leftField > rightField) return -1;
-//						if (leftField < rightField) return 1;
-//						return 0;
-//					}
-//				};
-//				break;
-//			case AppWideConstants.comparators.COMPARATOR_REQUEST_PRIORITY:
-//				comparator = new Comparator<AbstractPropertyChanger>() {
-//					public int compare(final AbstractPropertyChanger left, final AbstractPropertyChanger right) {
-//						long leftField = -1;
-//						long rightField = -1;
-//						if (left instanceof PendingRequestEntry) {
-//							final PendingRequestEntry intermediate = (PendingRequestEntry) left;
-//							leftField = intermediate.getPriority();
-//						}
-//
-//						if (right instanceof PendingRequestEntry) {
-//							final PendingRequestEntry intermediate = (PendingRequestEntry) right;
-//							rightField = intermediate.getPriority();
-//						}
-//						if (leftField < rightField) return -1;
-//						if (leftField > rightField) return 1;
-//						return 0;
-//					}
-//				};
-//				break;
-//			case AppWideConstants.comparators.COMPARATOR_PRIORITY:
-//				comparator = new Comparator<AbstractPropertyChanger>() {
-//					public int compare(final AbstractPropertyChanger left, final AbstractPropertyChanger right) {
-//						int leftField = -1;
-//						int rightField = -1;
-//						if (left instanceof PendingRequestEntry) {
-//							final PendingRequestEntry intermediate = (PendingRequestEntry) left;
-//							leftField = intermediate.getPriority();
-//						}
-//
-//						if (right instanceof PendingRequestEntry) {
-//							final PendingRequestEntry intermediate = (PendingRequestEntry) right;
-//							rightField = intermediate.getPriority();
-//						}
-//						if (leftField < rightField) return 1;
-//						if (leftField > rightField) return -1;
-//						return 0;
-//					}
-//				};
-//				break;
+			//			case AppWideConstants.comparators.COMPARATOR_APIID_ASC:
+			//				comparator = new Comparator<AbstractPropertyChanger>() {
+			//					public int compare(final AbstractPropertyChanger left, final AbstractPropertyChanger right) {
+			//						long leftField = -1;
+			//						long rightField = -1;
+			//						if (left instanceof APIKeyPart) {
+			//							final APIKey intermediate = ((APIKeyPart) left).getCastedModel();
+			//							leftField = intermediate.getKeyID();
+			//						}
+			//
+			//						if (right instanceof APIKeyPart) {
+			//							final APIKey intermediate = ((APIKeyPart) right).getCastedModel();
+			//							rightField = intermediate.getKeyID();
+			//						}
+			//						if (leftField < rightField) return -1;
+			//						if (leftField > rightField) return 1;
+			//						return 0;
+			//					}
+			//				};
+			//				break;
+			//			case AppWideConstants.comparators.COMPARATOR_APIID_DESC:
+			//				comparator = new Comparator<AbstractPropertyChanger>() {
+			//					public int compare(final AbstractPropertyChanger left, final AbstractPropertyChanger right) {
+			//						long leftField = -1;
+			//						long rightField = -1;
+			//						if (left instanceof APIKeyPart) {
+			//							final APIKey intermediate = ((APIKeyPart) left).getCastedModel();
+			//							leftField = intermediate.getKeyID();
+			//						}
+			//
+			//						if (right instanceof APIKeyPart) {
+			//							final APIKey intermediate = ((APIKeyPart) right).getCastedModel();
+			//							rightField = intermediate.getKeyID();
+			//						}
+			//						if (leftField > rightField) return -1;
+			//						if (leftField < rightField) return 1;
+			//						return 0;
+			//					}
+			//				};
+			//				break;
+			case REQUEST_PRIORITY:
+				comparator = new Comparator<AbstractPropertyChanger>() {
+					public int compare(final AbstractPropertyChanger left, final AbstractPropertyChanger right) {
+						long leftField = -1;
+						long rightField = -1;
+						if (left instanceof PendingRequestEntry) {
+							final PendingRequestEntry intermediate = (PendingRequestEntry) left;
+							leftField = intermediate.getPriority();
+						}
+
+						if (right instanceof PendingRequestEntry) {
+							final PendingRequestEntry intermediate = (PendingRequestEntry) right;
+							rightField = intermediate.getPriority();
+						}
+						if (leftField < rightField) return -1;
+						if (leftField > rightField) return 1;
+						return 0;
+					}
+				};
+				break;
+			//			case AppWideConstants.comparators.COMPARATOR_PRIORITY:
+			//				comparator = new Comparator<AbstractPropertyChanger>() {
+			//					public int compare(final AbstractPropertyChanger left, final AbstractPropertyChanger right) {
+			//						int leftField = -1;
+			//						int rightField = -1;
+			//						if (left instanceof PendingRequestEntry) {
+			//							final PendingRequestEntry intermediate = (PendingRequestEntry) left;
+			//							leftField = intermediate.getPriority();
+			//						}
+			//
+			//						if (right instanceof PendingRequestEntry) {
+			//							final PendingRequestEntry intermediate = (PendingRequestEntry) right;
+			//							rightField = intermediate.getPriority();
+			//						}
+			//						if (leftField < rightField) return 1;
+			//						if (leftField > rightField) return -1;
+			//						return 0;
+			//					}
+			//				};
+			//				break;
 			case WEIGHT:
 				comparator = new Comparator<AbstractPropertyChanger>() {
 					public int compare(final AbstractPropertyChanger left, final AbstractPropertyChanger right) {
@@ -273,74 +271,74 @@ public class ComparatorFactory {
 					}
 				};
 				break;
-//			case AppWideConstants.comparators.COMPARATOR_CARD_RATIO:
-//				comparator = new Comparator<AbstractPropertyChanger>() {
-//					public int compare(final AbstractPropertyChanger left, final AbstractPropertyChanger right) {
-//						double leftField = 0.0;
-//						double rightField = 0.0;
-//						// if (left instanceof ModulePart) {
-//						// ModuleCard intermediate = ((ModulePart)
-//						// left).getCastedModel();
-//						// leftField = intermediate.getModuleIndex();
-//						// }
-//						if (left instanceof BlueprintPart) {
-//							leftField = ((BlueprintPart) left).getProfitIndex();
-//						}
-//
-//						// if (right instanceof ModulePart) {
-//						// ModuleCard intermediate = ((ModulePart)
-//						// right).getCastedModel();
-//						// rightField = intermediate.getModuleIndex();
-//						// }
-//						if (right instanceof BlueprintPart) {
-//							rightField = ((BlueprintPart) right).getProfitIndex();
-//						}
-//
-//						if (leftField > rightField)
-//							return -1;
-//						else if (leftField == rightField) return 0;
-//						return 1;
-//					}
-//				};
-//				break;
-//			case AppWideConstants.comparators.COMPARATOR_NEWESTDATESORT:
-//				comparator = new Comparator<AbstractPropertyChanger>() {
-//					public int compare(final AbstractPropertyChanger left, final AbstractPropertyChanger right) {
-//						DateTime leftField = new DateTime(DateTimeZone.UTC);
-//						DateTime rightField = new DateTime(DateTimeZone.UTC);
-//						if (left instanceof IDateTimeComparator) {
-//							leftField = ((IDateTimeComparator) left).getComparableDate();
-//						}
-//						if (right instanceof IDateTimeComparator) {
-//							rightField = ((IDateTimeComparator) right).getComparableDate();
-//						}
-//
-//						if (leftField.isAfter(rightField))
-//							return -1;
-//						else
-//							return 1;
-//					}
-//				};
-//				break;
-//			case AppWideConstants.comparators.COMPARATOR_OLDESTDATESORT:
-//				comparator = new Comparator<AbstractPropertyChanger>() {
-//					public int compare(final AbstractPropertyChanger left, final AbstractPropertyChanger right) {
-//						DateTime leftField = new DateTime(DateTimeZone.UTC);
-//						DateTime rightField = new DateTime(DateTimeZone.UTC);
-//						if (left instanceof IDateTimeComparator) {
-//							leftField = ((IDateTimeComparator) left).getComparableDate();
-//						}
-//						if (right instanceof IDateTimeComparator) {
-//							rightField = ((IDateTimeComparator) right).getComparableDate();
-//						}
-//
-//						if (leftField.isAfter(rightField))
-//							return 1;
-//						else
-//							return -1;
-//					}
-//				};
-//				break;
+			//			case AppWideConstants.comparators.COMPARATOR_CARD_RATIO:
+			//				comparator = new Comparator<AbstractPropertyChanger>() {
+			//					public int compare(final AbstractPropertyChanger left, final AbstractPropertyChanger right) {
+			//						double leftField = 0.0;
+			//						double rightField = 0.0;
+			//						// if (left instanceof ModulePart) {
+			//						// ModuleCard intermediate = ((ModulePart)
+			//						// left).getCastedModel();
+			//						// leftField = intermediate.getModuleIndex();
+			//						// }
+			//						if (left instanceof BlueprintPart) {
+			//							leftField = ((BlueprintPart) left).getProfitIndex();
+			//						}
+			//
+			//						// if (right instanceof ModulePart) {
+			//						// ModuleCard intermediate = ((ModulePart)
+			//						// right).getCastedModel();
+			//						// rightField = intermediate.getModuleIndex();
+			//						// }
+			//						if (right instanceof BlueprintPart) {
+			//							rightField = ((BlueprintPart) right).getProfitIndex();
+			//						}
+			//
+			//						if (leftField > rightField)
+			//							return -1;
+			//						else if (leftField == rightField) return 0;
+			//						return 1;
+			//					}
+			//				};
+			//				break;
+			//			case AppWideConstants.comparators.COMPARATOR_NEWESTDATESORT:
+			//				comparator = new Comparator<AbstractPropertyChanger>() {
+			//					public int compare(final AbstractPropertyChanger left, final AbstractPropertyChanger right) {
+			//						DateTime leftField = new DateTime(DateTimeZone.UTC);
+			//						DateTime rightField = new DateTime(DateTimeZone.UTC);
+			//						if (left instanceof IDateTimeComparator) {
+			//							leftField = ((IDateTimeComparator) left).getComparableDate();
+			//						}
+			//						if (right instanceof IDateTimeComparator) {
+			//							rightField = ((IDateTimeComparator) right).getComparableDate();
+			//						}
+			//
+			//						if (leftField.isAfter(rightField))
+			//							return -1;
+			//						else
+			//							return 1;
+			//					}
+			//				};
+			//				break;
+			//			case AppWideConstants.comparators.COMPARATOR_OLDESTDATESORT:
+			//				comparator = new Comparator<AbstractPropertyChanger>() {
+			//					public int compare(final AbstractPropertyChanger left, final AbstractPropertyChanger right) {
+			//						DateTime leftField = new DateTime(DateTimeZone.UTC);
+			//						DateTime rightField = new DateTime(DateTimeZone.UTC);
+			//						if (left instanceof IDateTimeComparator) {
+			//							leftField = ((IDateTimeComparator) left).getComparableDate();
+			//						}
+			//						if (right instanceof IDateTimeComparator) {
+			//							rightField = ((IDateTimeComparator) right).getComparableDate();
+			//						}
+			//
+			//						if (leftField.isAfter(rightField))
+			//							return 1;
+			//						else
+			//							return -1;
+			//					}
+			//				};
+			//				break;
 		}
 		return comparator;
 	}
