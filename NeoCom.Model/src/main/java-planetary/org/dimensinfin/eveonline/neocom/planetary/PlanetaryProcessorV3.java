bdf0377cf826261f10fa2fc3cfbf017618bba4e1
Adam Antinoo <adamantinoo.git@gmail.com>
2018-05-24 17:42:25 +0200

    -[WIP] New update to the latest algorithm but still work on progress.

--- NeoCom.Model/src/main/java-planetary/org/dimensinfin/eveonline/neocom/planetary/PlanetaryProcessorV3.java
+++ NeoCom.Model/src/main/java-planetary/org/dimensinfin/eveonline/neocom/planetary/PlanetaryProcessorV3.java
@@ -29,6 +29,12 @@ import org.dimensinfin.eveonline.neocom.industry.Resource;
 public class PlanetaryProcessorV3 extends PlanetaryProcessorV2 {
 	// - S T A T I C - S E C T I O N ..........................................................................
 	private static Logger logger = LoggerFactory.getLogger("PlanetaryProcessorV3");
+	private static final int Tier3Resources4OneUnit = 6;
+	private static final int Tier3ResourcesByCycle = 3;
+	private static final int Tier2Resources4OneUnit = 10;
+	private static final int Tier2ResourcesByCycle = 5;
+	private static final int Tier1Resources4OneUnit = 40;
+	private static final int Tier1ResourcesByCycle = 20;
 
 	// - F I E L D - S E C T I O N ............................................................................
 
@@ -62,26 +68,49 @@ public class PlanetaryProcessorV3 extends PlanetaryProcessorV2 {
 			final List<Resource> requirementsTier3 = addProductsRequired(typeId);
 			// Tier 3 need 6 resources to initiate a cycle.
 			for (Resource res3 : requirementsTier3) {
-				processStorage.addTargetAvailable(res3.getTypeId(), scenery.getResource(typeId).getQuantity());
-				processStorage.addRequirement(res3.getTypeId(), 6);
-				Resource available = scenery.getResource(res3.getTypeId());
-				processStorage.addAvailable(res3.getTypeId(), available.getQuantity());
+				// Calculate the requirements for this resource depending on its target availability and current availabilities.
+				// We calculate a 100 units Tier 4 to have simple maths when going down.
+				final int TIER4_MULTIPLIER = 100;
+				final int tier3Required = Tier3Resources4OneUnit * TIER4_MULTIPLIER;
+				final int tier4Available = scenery.getResource(typeId).getQuantity();
+				final int tier3RequestQuantity = tier3Required - (tier4Available * Tier3ResourcesByCycle);
+				final int tier3Available = scenery.getResource(res3.getTypeId()).getQuantity();
 
-				// Do the same searching for input for this Tier 3 resources.
-				final List<Resource> requirementsTier2 = addProductsRequired(res3.getTypeId());
-				for (Resource res2 : requirementsTier2) {
-					processStorage.addTargetAvailable(res2.getTypeId(), scenery.getResource(res3.getTypeId()).getQuantity());
-					processStorage.addRequirement(res2.getTypeId(), 10);
-					available = scenery.getResource(res2.getTypeId());
-					processStorage.addAvailable(res2.getTypeId(), available.getQuantity());
+				// Generate a request only if we have less than required.
+				if (tier3Available < tier3RequestQuantity) {
+					// Store the Tier 3 accounting data into a new ResourceStorage.
+					processStorage.addRequirement(res3.getTypeId(), tier3RequestQuantity);
+					processStorage.addAvailable(res3.getTypeId(), scenery.getResource(res3.getTypeId()).getQuantity());
 
-					// And again for the resulting Tier 2 resources.
-					final List<Resource> requirementsTier1 = addProductsRequired(res2.getTypeId());
-					for (Resource res1 : requirementsTier1) {
-						processStorage.addTargetAvailable(res1.getTypeId(), scenery.getResource(res2.getTypeId()).getQuantity());
-						processStorage.addRequirement(res1.getTypeId(), 40);
-						available = scenery.getResource(res1.getTypeId());
-						processStorage.addAvailable(res1.getTypeId(), available.getQuantity());
+					// Do the same operation with this Tier 3 and generate the requirements from Tier 2.
+					final List<Resource> requirementsTier2 = addProductsRequired(res3.getTypeId());
+					for (Resource res2 : requirementsTier2) {
+						final int tier2Required = tier3RequestQuantity * Tier2Resources4OneUnit;
+						final int tier2RequestQuantity = tier2Required - (tier3Available * Tier2ResourcesByCycle);
+						final int tier2Available = scenery.getResource(res2.getTypeId()).getQuantity();
+
+						// Generate a request only if we have less than required.
+						if (tier2Available < tier2RequestQuantity) {
+							// Store the Tier 2 accounting data into a new ResourceStorage.
+							processStorage.addRequirement(res2.getTypeId(), tier2RequestQuantity);
+							processStorage.addAvailable(res2.getTypeId(), scenery.getResource(res2.getTypeId()).getQuantity());
+
+							// Do the same operation with this Tier 2 and generate the requirements from Tier 1.
+							final List<Resource> requirementsTier1 = addProductsRequired(res2.getTypeId());
+							for (Resource res1 : requirementsTier1) {
+								final int tier1Required = tier2RequestQuantity * Tier1Resources4OneUnit;
+								final int tier1RequestQuantity = tier1Required - (tier2Available * Tier1ResourcesByCycle);
+								final int tier1Available = scenery.getResource(res1.getTypeId()).getQuantity();
+
+								// Generate a request only if we have less than required.
+								if (tier1Available < tier1RequestQuantity) {
+									// Store the Tier 2 accounting data into a new ResourceStorage.
+									processStorage.addRequirement(res1.getTypeId(), tier1RequestQuantity);
+									processStorage.addAvailable(res1.getTypeId(), scenery.getResource(res1.getTypeId()).getQuantity());
+									//[01]
+								}
+							}
+						}
 					}
 				}
 			}
@@ -89,6 +118,29 @@ public class PlanetaryProcessorV3 extends PlanetaryProcessorV2 {
 			// With this we can calculate the Tier 4 cycle cut and reduce it from 100 to the real level.
 
 		}
+//[01]
+		//									processStorage.addTargetAvailable(res3.getTypeId(), scenery.getResource(typeId).getQuantity());
+//
+//									// Do the same searching for input for this Tier 3 resources.
+//									// Calculate the requirements for this resource depending on its target availability.
+//									final int required = 10 * 100;
+//									final int currentAvailable = scenery.getResource(typeId).getQuantity());
+//									// Generate a request only if we have less that required.
+//									if (currentAvailable < required) {
+//										processStorage.addTargetAvailable(res2.getTypeId(), scenery.getResource(res3.getTypeId()).getQuantity());
+//										processStorage.addRequirement(res2.getTypeId(), 10);
+//										available = scenery.getResource(res2.getTypeId());
+//										processStorage.addAvailable(res2.getTypeId(), available.getQuantity());
+//
+//										// And again for the resulting Tier 2 resources.
+//										final List<Resource> requirementsTier1 = addProductsRequired(res2.getTypeId());
+//										for (Resource res1 : requirementsTier1) {
+//											processStorage.addTargetAvailable(res1.getTypeId(), scenery.getResource(res2.getTypeId()).getQuantity());
+//											processStorage.addRequirement(res1.getTypeId(), 40);
+//											available = scenery.getResource(res1.getTypeId());
+//											processStorage.addAvailable(res1.getTypeId(), available.getQuantity());
+//										}
+//									}
 
 
 		PlanetaryProcessor.logger.info("<< [PlanetaryProcessorV3.startProfitSearch]");
@@ -97,7 +149,7 @@ public class PlanetaryProcessorV3 extends PlanetaryProcessorV2 {
 
 	// --- D E L E G A T E D   M E T H O D S
 	public static class ProcessStorage {
-		private static final int INITIAL_T4_MULTIPLIER = 100;
+		//		private static final int INITIAL_T4_MULTIPLIER = 100;
 		private final int targetTypeId;
 		private final PlanetaryScenery scenery;
 		private Map<Integer, ResourceStorage> resources = new HashMap();
@@ -107,14 +159,14 @@ public class PlanetaryProcessorV3 extends PlanetaryProcessorV2 {
 			this.scenery = scenery;
 		}
 
-		public void addRequirement( final int typeId, final int resourceMultiplier ) {
+		public void addRequirement( final int typeId, final int requestQuantity ) {
 			ResourceStorage hit = resources.get(typeId);
 			if (null == hit) {
 				// Create the storage for a new resource.
 				hit = new ResourceStorage(typeId);
 				resources.put(typeId, hit);
 			}
-			hit.addRequirement(new PlanetaryResource(typeId, resourceMultiplier, INITIAL_T4_MULTIPLIER));
+			hit.addRequirement(new PlanetaryResource(typeId, requestQuantity));
 		}
 
 		public void addAvailable( final int typeId, final int quantity ) {
@@ -174,7 +226,7 @@ public class PlanetaryProcessorV3 extends PlanetaryProcessorV2 {
 		@Override
 		public String toString() {
 			return new StringBuffer("ResourceStorage [ ")
-					.append("[").append(requirement.getTypeId()).append("]").append(requirement.getName()).append(" ")
+					.append("[").append(requirement.getTier()).append("]").append(requirement.getName()).append(" ")
 					.append("required: ").append(requirement.getQuantity()).append(" ")
 					.append("available: ").append(available.getQuantity()).append(" ")
 					.append("]")
