4d5852f0d37f3fb8ed8959ed58abeb464d08cf14
Adam Antinoo <adamantinoo.git@gmail.com>
2019-10-25 19:20:57 +0200

    -[DATAMANAGEMENT] Completed some new access methods on the Corporation class.

--- NeoCom.DataManagement/src/main/java-domain/org/dimensinfin/eveonline/neocom/domain/Corporation.java
+++ NeoCom.DataManagement/src/main/java-domain/org/dimensinfin/eveonline/neocom/domain/Corporation.java
@@ -18,7 +18,7 @@ public class Corporation extends UpdatableNode {
 	private static final long serialVersionUID = -2990989673090493394L;
 	private static final long CORPORATION_CACHE_TIME = TimeUnit.HOURS.toMillis( 12 );
 
-	private int corporationId;
+	private Integer corporationId;
 	private GetCorporationsCorporationIdOk corporationPublicData;
 	private Pilot ceoPilotData;
 	private GetAlliancesAllianceIdOk alliance;
@@ -35,6 +35,7 @@ public class Corporation extends UpdatableNode {
 	}
 
 	// - I C O L L A B O R A T I O N
+
 	/**
 	 * Return the elements collaborated by this object. For a Character it depends on the implementation being a
 	 * Pilot or a Corporation. For a Pilot the result depends on the variant received as the parameter
@@ -45,20 +46,33 @@ public class Corporation extends UpdatableNode {
 		return results;
 	}
 
+	// - V I R T U A L S
+	public String getUrl4Icon() {
+		return "http://image.eveonline.com/Corporation/" + this.corporationId + "_64.png";
+	}
+
 	// - G E T T E R S   &   S E T T E R S
-	public int getCorporationId() {
+	public Integer getCorporationId() {
 		return corporationId;
 	}
 
+	public GetCorporationsCorporationIdOk getCorporationPublicData() {
+		return this.corporationPublicData;
+	}
+
 	public Pilot getCeoPilotData() {
-		return ceoPilotData;
+		return this.ceoPilotData;
+	}
+
+	public GetAlliancesAllianceIdOk getAlliance() {
+		return this.alliance;
 	}
 
 	// - D E L E G A T E D
-	public int getAllianceId (){
+	public Integer getAllianceId() {
 		if (null != this.corporationPublicData)
 			return this.corporationPublicData.getAllianceId();
-		else return -1;
+		else return -1; // This code is unreacheable.
 	}
 
 	// - B U I L D E R
@@ -74,22 +88,26 @@ public class Corporation extends UpdatableNode {
 			this.onConstruction.corporationId = corporationId;
 			return this;
 		}
+
 		public Corporation.Builder withCorporationPublicData( final GetCorporationsCorporationIdOk corporationPublicData ) {
 			Objects.requireNonNull( corporationPublicData );
 			this.onConstruction.corporationPublicData = corporationPublicData;
 			return this;
 		}
+
 		public Corporation.Builder withCeoPilotData( final Pilot ceoPilotData ) {
-			Objects.requireNonNull(ceoPilotData);
+			Objects.requireNonNull( ceoPilotData );
 			this.onConstruction.ceoPilotData = ceoPilotData;
 			return this;
 		}
+
 		public Corporation.Builder optionslAlliance( final GetAlliancesAllianceIdOk alliance ) {
 			this.onConstruction.alliance = alliance;
 			return this;
 		}
 
 		public Corporation build() {
+			Objects.requireNonNull( this.onConstruction.corporationId );
 			Objects.requireNonNull( this.onConstruction.corporationPublicData );
 			Objects.requireNonNull( this.onConstruction.ceoPilotData );
 			return this.onConstruction;
--- NeoCom.DataManagement/src/test/java/org/dimensinfin/eveonline/neocom/domain/CorporationTest.java
+++ NeoCom.DataManagement/src/test/java/org/dimensinfin/eveonline/neocom/domain/CorporationTest.java
@@ -1,16 +1,21 @@
 package org.dimensinfin.eveonline.neocom.domain;
 
+import java.util.List;
+
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
+import org.dimensinfin.core.interfaces.ICollaboration;
 import org.dimensinfin.eveonline.neocom.database.entities.Credential;
+import org.dimensinfin.eveonline.neocom.esiswagger.model.GetAlliancesAllianceIdOk;
 import org.dimensinfin.eveonline.neocom.esiswagger.model.GetCharactersCharacterIdOk;
 import org.dimensinfin.eveonline.neocom.esiswagger.model.GetCorporationsCorporationIdOk;
 
 public class CorporationTest {
-	private Corporation corporation4Test;
+	private Corporation corporationNoAlliance;
+	private Corporation corporationInAlliance;
 	private GetCorporationsCorporationIdOk publicData;
 	private Pilot ceo;
 
@@ -21,6 +26,7 @@ public class CorporationTest {
 		publicData.setCeoId( 92223647 );
 		final GetCharactersCharacterIdOk ceoPublicData = Mockito.mock( GetCharactersCharacterIdOk.class );
 		final Credential credential = Mockito.mock( Credential.class );
+		final GetAlliancesAllianceIdOk alliance = Mockito.mock( GetAlliancesAllianceIdOk.class );
 		ceo = new Pilot.Builder()
 				.withPilotIdentifier( 123456 )
 				.withCredential( credential )
@@ -29,19 +35,47 @@ public class CorporationTest {
 				.withAncestryData( null )
 				.withBloodlineData( null )
 				.build();
-		corporation4Test = new Corporation.Builder()
+		corporationNoAlliance = new Corporation.Builder()
 				.withCorporationId( 98384726 )
 				.withCorporationPublicData( publicData )
 				.withCeoPilotData( ceo )
+				.build();
+		corporationInAlliance = new Corporation.Builder()
+				.withCorporationId( 98384726 )
+				.withCorporationPublicData( publicData )
+				.withCeoPilotData( ceo )
+				.optionslAlliance( alliance )
 				.build();
 	}
 
 	@Test
 	public void gettersContract() {
-		Assert.assertNotNull( corporation4Test );
-		Assert.assertNotNull( corporation4Test.getCeoPilotData() );
-		Assert.assertEquals( 98384726, corporation4Test.getCorporationId() );
-		Assert.assertEquals( 117383987, corporation4Test.getAllianceId() );
-		Assert.assertEquals( 123456, corporation4Test.getCeoPilotData().getPilotId() );
+		Assert.assertNotNull( corporationNoAlliance );
+		Assert.assertNotNull( corporationNoAlliance.getCorporationPublicData() );
+		Assert.assertNotNull( corporationNoAlliance.getCeoPilotData() );
+		Assert.assertNull( corporationNoAlliance.getAlliance() );
+		Assert.assertEquals( 98384726, corporationNoAlliance.getCorporationId().intValue() );
+		Assert.assertEquals( 117383987, corporationNoAlliance.getAllianceId().intValue() );
+		Assert.assertEquals( 123456, corporationNoAlliance.getCeoPilotData().getPilotId() );
+
+		Assert.assertNotNull( corporationInAlliance );
+		Assert.assertNotNull( corporationInAlliance.getCorporationPublicData() );
+		Assert.assertNotNull( corporationInAlliance.getCeoPilotData() );
+		Assert.assertNotNull( corporationInAlliance.getAlliance() );
+		Assert.assertEquals( "http://image.eveonline.com/Corporation/98384726_64.png",
+				corporationInAlliance.getUrl4Icon() );
+	}
+
+	@Test
+	public void needsRefresh() {
+		Assert.assertTrue( corporationInAlliance.needsRefresh() );
+		corporationNoAlliance.timeStamp();
+		Assert.assertFalse( corporationNoAlliance.needsRefresh() );
+	}
+
+	@Test
+	public void collaborate2Model() {
+		final List<ICollaboration> collaboration = corporationInAlliance.collaborate2Model( "-TEST-VARIANT-" );
+		Assert.assertTrue( collaboration.size() == 0 );
 	}
 }
