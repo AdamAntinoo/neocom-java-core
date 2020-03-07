5c7c0ffd26f3b15b59fd7278370077b1190b169b
Adam Antinoo <adamantinoo.git@gmail.com>
2020-03-07 01:47:19 +0100

    -[REFACTOR] This class is going to be removed.

--- NeoCom.DataManagement/src/main/java/org/dimensinfin/eveonline/neocom/backend/rest/v1/CredentialStoreResponse.java
+++ NeoCom.DataManagement/src/main/java/org/dimensinfin/eveonline/neocom/backend/rest/v1/CredentialStoreResponse.java
@@ -1,5 +1,7 @@
 package org.dimensinfin.eveonline.neocom.backend.rest.v1;
 
+import java.util.Objects;
+
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
@@ -10,5 +12,24 @@ public class CredentialStoreResponse {
 
 	public String getJwtToken() {
 		return jwtToken;
+	}
+
+	// - B U I L D E R
+	public static class Builder {
+		private CredentialStoreResponse onConstruction;
+
+		public Builder() {
+			this.onConstruction = new CredentialStoreResponse();
+		}
+
+		public CredentialStoreResponse build() {
+			return this.onConstruction;
+		}
+
+		public CredentialStoreResponse.Builder withJwtToken( final String jwtToken ) {
+			Objects.requireNonNull( jwtToken );
+			this.onConstruction.jwtToken = jwtToken;
+			return this;
+		}
 	}
 }
