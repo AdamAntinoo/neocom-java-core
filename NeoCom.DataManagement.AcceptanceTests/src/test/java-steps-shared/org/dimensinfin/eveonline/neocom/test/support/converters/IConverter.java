3aede87efd7d5372690b325509ac9e3d1bc3a900
Adam Antinoo <adamantinoo.git@gmail.com>
2020-03-07 01:29:27 +0100

    -[PROJECT] Removed the obsolete modules Support and Acceptance

--- NeoCom.DataManagement.AcceptanceTests/src/test/java-steps-shared/org/dimensinfin/eveonline/neocom/test/support/converters/IConverter.java
+++
@@ -1,5 +0,0 @@
-package org.dimensinfin.eveonline.neocom.test.support.converters;
-
-public interface IConverter<F, T> {
-	T convert(F input);
-}
--- NeoCom.DataManagement.AcceptanceTests/src/test/java-steps/org/dimensinfin/eveonline/neocom/steps/multisteps/DM04MiningRepository.java
+++
@@ -1,4 +0,0 @@
-package org.dimensinfin.eveonline.neocom.steps.multisteps;
-
-public class DM04MiningRepository {
-}
