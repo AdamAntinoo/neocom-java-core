c88b8907c7464ec2e28bd425d10248a2349c1eee
Adam Antinoo <adamantinoo.git@gmail.com>
2020-03-16 20:53:38 +0100

    -[MINING] Removing delta because it is not in use and deprecated.

--- NeoCom.DataManagement/src/main/java/org/dimensinfin/eveonline/neocom/miningextraction/domain/MiningExtraction.java
+++ NeoCom.DataManagement/src/main/java/org/dimensinfin/eveonline/neocom/miningextraction/domain/MiningExtraction.java
@@ -21,7 +21,6 @@ import org.dimensinfin.eveonline.neocom.domain.space.SpaceSystem;
 @JsonIgnoreProperties
 public class MiningExtraction {
 	private long quantity = 0L;
-	private long delta = 0;
 	private String extractionDateName;
 	private int extractionHour = 24; // The hour of the day for this extraction delta or 24 if this is the date aggregated value.
 	private Integer ownerId; // The credential identifier of the pilot's extraction.
@@ -94,13 +93,17 @@ public class MiningExtraction {
 		return this.resourceItem.getPrice();
 	}
 
+	public MiningExtraction setQuantity( final long quantity ) {
+		this.quantity = quantity;
+		return this;
+	}
+
 	// - C O R E
 	@Override
 	public int hashCode() {
 		return new HashCodeBuilder( 17, 37 )
 				.append( this.getId() )
 				.append( this.quantity )
-				.append( this.delta )
 				.append( this.extractionDateName )
 				.append( this.extractionHour )
 				.append( this.ownerId )
@@ -117,7 +120,6 @@ public class MiningExtraction {
 		return new EqualsBuilder()
 				.append( this.getId(), that.getId() )
 				.append( this.quantity, that.quantity )
-				.append( this.delta, that.delta )
 				.append( this.extractionHour, that.extractionHour )
 				.append( this.ownerId, that.ownerId )
 				.append( this.extractionDateName, that.extractionDateName )
@@ -130,7 +132,6 @@ public class MiningExtraction {
 	public String toString() {
 		return new ToStringBuilder( this, ToStringStyle.JSON_STYLE )
 				.append( "quantity", this.quantity )
-				.append( "delta", this.delta )
 				.append( "extractionDateName", this.extractionDateName )
 				.append( "extractionHour", this.extractionHour )
 				.append( "ownerId", this.ownerId )
